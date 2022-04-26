d5dc98902da92674d2095a2c4d9adad0511405b7
wandergithub <wandergonzalez0@hotmail.com>
2022-04-26 14:28:24 -0400

    index files update

---
+++ index.js
@@ -0,0 +1,19 @@
+import Storage from './modules/storage.js';
+import BookList, {
+  listOfName
+} from './modules/BookList.js';
+import createBooklist from "./modules/createBooklist.js";
+import {btnClass, links} from "./modules/htmlElements.js";
+import sectionsHandler from "./modules/sectionsHandler.js";
+import removeBook from './modules/removeBook.js';
+
+btnClass.addEventListener('click', () => {
+    createBooklist();
+});
+window.addEventListener('load', BookList.display());
+links.addEventListener('click', (element) => {
+  sectionsHandler(element);
+});
+listOfName.addEventListener('click', (element) => {
+  removeBook(element);
+});
